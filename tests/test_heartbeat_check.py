9f6d634968ccfd968b2650dbb8d6878cc6be7762
Muneeb H. Jan <muneebhafeezjan@gmail.com>
2024-07-11 10:44:19 +0200

    [Test] Add new failure case

--- tests/test_heartbeat_check.py
+++ tests/test_heartbeat_check.py
@@ -1,5 +1,6 @@
 import unittest
 from unittest.mock import patch
+import requests
 from globalmon.utils import heartbeat_check
 
 
@@ -14,22 +15,45 @@ class TestHeartbeatCheck(unittest.TestCase):
         result = heartbeat_check(services)
         expected_result = {
             'example_service': {
-                # Expected response time
                 'https://example.com': {'return_code': 200, 'return_time': 0}
             }
         }
         self.assertEqual(result, expected_result)
 
     @patch('globalmon.utils.requests.get')
-    def test_heartbeat_check_failure(self, mock_get):
-        mock_get.side_effect = Exception("Connection Error")
+    def test_heartbeat_check_timeout(self, mock_get):
+        mock_get.side_effect = requests.exceptions.Timeout
         services = {
-            'example_service': {'urls': ['https://example.com']}
+            'example_service': {
+                'urls': ['https://example.com']
+                }
         }
-        with self.assertRaises(Exception) as context:
-            heartbeat_check(services)
+        result = heartbeat_check(services)
+        expected_result = {'example_service': {
+            'https://example.com': {
+                'return_code': 'timeout', 'return_time': 10
+                }
+            }
+        }
+        self.assertEqual(result, expected_result)
 
-        self.assertEqual(str(context.exception), "Connection Error")
+    @patch('globalmon.utils.requests.get')
+    def test_heartbeat_check_failure(self, mock_get):
+        mock_get.side_effect = requests.exceptions.RequestException(
+            "Connection Error")
+        services = {
+            'example_service': {
+                'urls': ['https://example.com']
+            }
+        }
+        result = heartbeat_check(services)
+        expected_result = {'example_service': {
+            'https://example.com': {
+                'return_code': 'failed', 'return_time': 0
+                }
+            }
+        }
+        self.assertEqual(result, expected_result)
 
 
 if __name__ == '__main__':
